af84caec38e4827d1c23f44fee23b19b9c7593e7
HawukaGIT <ucho1985@o2.pl>
2023-10-25 18:30:09 +0200

    https server

--- index.js
+++ index.js
@@ -9,10 +9,12 @@ const PORT = process.env.PORT || 80;
 await dbconnect;
 //await import('./db/mongoose.js');
 
+//https server
 https.createServer(app).listen(PORT, () => {
   console.log(`server is runing at port ${PORT}`);
 });
 
+//http server
 // app.listen(PORT, () => {
 //   console.log(`serwer słucha na porcie ${PORT}`);
 // });
