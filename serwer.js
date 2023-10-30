8f85ee2e90c601e259cd1091af7a7783edac176b
HawukaGIT <ucho1985@o2.pl>
2023-10-30 10:23:49 +0100

    ssl trying

--- serwer.js
+++ serwer.js
@@ -5,13 +5,12 @@ import cookieParser from "cookie-parser";
 import dotenv from "dotenv";
 dotenv.config();
 
-
 const app = express();
 
 app.use(
   cors({
     credentials: true,
-    origin: ["http://localhost:3000"],
+    origin: ["http://localhost:3000", "https://localhost:3000"],
   })
 );
 app.use(express.json());
